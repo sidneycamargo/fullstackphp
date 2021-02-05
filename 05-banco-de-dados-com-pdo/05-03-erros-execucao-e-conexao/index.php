72c308952b72d477de0abed24502c4155ebf155c
Sidney Camargo Moraes <sidneycamargo@gmail.Camargo>
2021-02-05 18:24:36 -0300

    Primeiro Commit Curso FullStackPHP

--- 05-banco-de-dados-com-pdo/05-03-erros-execucao-e-conexao/index.php
+++ 05-banco-de-dados-com-pdo/05-03-erros-execucao-e-conexao/index.php
@@ -7,6 +7,18 @@ fullStackPHPClassName("05.03 - Errors, conexão e execução");
  */
 fullStackPHPClassSession("controle de erros", __LINE__);
 
+try {
+    throw new Exception("Exception");
+    throw new PDOException("Exception");
+    throw new ErrorException("ErrorException", 1);
+
+} catch (PDOException | ErrorException $exception) {
+    var_dump($exception);
+} catch (Exception  $exception) {
+    echo "<p class='trigger error'>{$exception->getMessage()}</>";
+} finally {
+    echo "<p class='trigger'>Execução terminou!</p>";
+}
 
 /*
  * [ php data object ] Uma classe PDO para manipulação de banco de dados.
@@ -14,6 +26,18 @@ fullStackPHPClassSession("controle de erros", __LINE__);
  */
 fullStackPHPClassSession("php data object", __LINE__);
 
+try {
+    $pdo = new PDO (
+        "mysql:host=localhost;dbname=fullstackphp",
+        "root",
+        "",
+        [
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+        ]
+    );
+} catch (PDOException $exception) {
+    var_dump($exception);
+}
 
 /*
  * [ conexão com singleton ] Conextar e obter um objeto PDO garantindo instância única.
